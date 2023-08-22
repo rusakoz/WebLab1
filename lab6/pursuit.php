6cf2bdca45d6fcd1b35b79dee50e9da4d4275907
Ruslan <julyrak@gmail.com>
2023-08-22 22:14:08 +0300

    lab6

---
+++ lab6/pursuit.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<?php
+include("../path.php");
+include "../logic/DB/database.php";
+?>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/f8998f952b.js" crossorigin="anonymous"></script>
+    <script src="pursuit.js" defer> </script> <!-- defer - запуск скрипта после загрузки html страницы -->
+</head>
+<body>
+
+<!--Header-->
+<?php include("../logic/Include/Header.php"); ?>
+<!--Header-->
+
+<!--Main-->
+<div id="main-expert" class="main container-fluid">
+    <div id="expert-button" class="main-content container">
+        <div class="row" id="super">
+            <!--<canvas id="game" width="600" height="600">-->
+            <div class="col">
+                <h3 id="textTimer"></h3>
+            </div>
+            <div class="col">
+                <form name="form1">
+                    <select class="form-select" aria-label="Пример выбора по умолчанию" id="timer">
+                        <option disabled>Время прохождения теста</option>
+                        <option value="10">10сек</option>
+                        <option value="60">1 минута</option>
+                        <option value="900">15 минут</option>
+                    </select>
+                    <button type="submit" class="btn btn-secondary" name="button-lab4-start" style="margin-top: 150px; margin-left: 50px"> Начать тестирование</button>
+                </form>
+            </div>
+            <div class="col">
+            </div>
+            <!--</canvas>-->
+        </div>
+    </div>
+</div>
+<!--Main-->
+
+<!--Footer-->
+<?php include("../logic/Include/Footer.php"); ?>
+<!--Footer-->
+
+
+</body>
+</html>
---
+++ lab6/stat.js
@@ -0,0 +1,19 @@
+
+function processStat(){
+    Promise.all([
+        getResultsInfo('../logic/DB/resultInfo/selectAll.php', 'resultlight', idSession),
+        getResultsInfo('../logic/DB/resultInfo/selectAll.php', 'resultaddition', idSession),
+        getResultsInfo('../logic/DB/resultInfo/selectAll.php', 'resultsound', idSession),
+        getResultsInfo('../logic/DB/resultInfo/selectAll.php', 'resulthardsound', idSession),
+        getResultsInfo('../logic/DB/resultInfo/selectAll.php', 'resultthreelight', idSession),
+        getResultsInfo('../logic/DB/resultInfo/selectAll.php', 'resultlab4-1', idSession),
+        getResultsInfo('../logic/DB/resultInfo/selectAll.php', 'resultlab4-2', idSession),
+        getResultsInfo('../logic/DB/resultInfo/selectAll.php', 'resultlab5-1', idSession),
+        getResultsInfo('../logic/DB/resultInfo/selectAll.php', 'resultlab5-2', idSession),
+        getResultsInfo('../logic/DB/resultInfo/selectAll.php', 'resultlab5-3', idSession)
+    ]).then((result)=>{
+        result.forEach((a) => {console.log(a)})
+    }).catch(()=> alert('ошибка'))
+}
+
+processStat();
